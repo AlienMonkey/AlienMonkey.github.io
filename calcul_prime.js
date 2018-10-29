107cd016b709ebb37defbcee636abc8e7d92332b
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-10-29 10:56:12 +0200

    zcxvc

--- calcul_prime.js
+++ calcul_prime.js
@@ -7,7 +7,7 @@ function is_prim(n)
 	return true;	
 }
 //-------------------------------------------
-for (var i = 1; i < 10000; i++)
+for (var i = 1; i < 1e8; i++)
 	if (is_prim(i))
 		postMessage(i);
 	
