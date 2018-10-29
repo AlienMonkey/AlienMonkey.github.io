8d84c0c4b6d6030906de6686d2d95ce83d0d2226
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-10-29 12:50:57 +0200

    kj

--- device_or.js
+++ device_or.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.10.29.1";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.10.29.2";
 window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(e){
@@ -11,6 +11,7 @@ function on_device_orientation(e){
 	context.clearRect(0, 0, canvas.width/2, canvas.height/2);
 	var R=10;
 	context.beginPath();
-	context.arc(canvas.width/2+e.gamma/90*(canvas.width/2-R), canvas.height/2+e.beta/90*(canvas.height/2-R), 10, 0, 2*Math.PI);
+	context.arc(canvas.width/2+e.gamma/90*(canvas.width/2-R),
+				canvas.height/2+e.beta/90*(canvas.height/2-R), 10, 0, 2*Math.PI);
 	context.stroke();
 }
