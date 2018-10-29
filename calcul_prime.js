872e6244685da11498c0eb281b256ffe6a39e3bf
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-10-29 10:42:57 +0200

    ssafsgdhjgh

--- calcul_prime.js
+++ calcul_prime.js
@@ -1,12 +1,11 @@
 function is_prim(n){
-	for (var i=2; i*i<=n; i++)
-		if(n%i==0)
-			return false;
+	for (var i=2; i*i<=n; i++){
+		if(n%i==0) return false;
 	return true;
+	}
 }
 
-for (var i=1e9; i<1e10; i++)
-	if(is_prim(i))
-		postMessage(i);
-
+for (var i=1e9; i<1e10; i++){
+	if(is_prim(i)) postMessage(i);
+}
 postMessage("gata");
