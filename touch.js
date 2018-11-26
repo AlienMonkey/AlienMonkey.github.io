2cf67cab2e22e44373746f6458123febea20cc14
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-11-26 11:28:38 +0200

    wert

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.2";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.3";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
