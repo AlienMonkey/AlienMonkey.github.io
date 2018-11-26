d97a63238cf22932b0a9346456df78ab87f871d1
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-11-26 11:43:13 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -9,6 +9,7 @@ var canvas_boundig_rect = canvas.getBoundingClientRect();
 var last_pos = {x: 0, y: 0};
 
 function on_touch_start(e){
+	e.preventDefault();
 	for(var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
@@ -17,6 +18,8 @@ function on_touch_start(e){
 		context.arc(e.changedTouches[i].pageX - canvas_boundig_rect.left,
 					e.changedTouches[i].pageY - canvas_boundig_rect.top, 10, 0, 2 * Math.PI);
 		context.stroke();
+		last_pos.x = e.changedTouches[i].pageX
+		last_pos.y = e.changedTouches[i].pageY
 	}
 }
 
