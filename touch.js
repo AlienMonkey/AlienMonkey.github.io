848b6b57a805b9100005eb13f39c02df0d51fcda
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-11-26 11:44:04 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.5";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.6";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
