1861c180760bda8e53fd2bb716827f45b8c5b522
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-11-26 11:06:10 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,10 +1,10 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.0";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.1";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 
 function on_touch_start(e){
-	for(var i = 0; i < e.changeTouches.length; i++){
+	for(var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
 		context.arc(e.changedTouches[i].pageX, e.changedTouches[i].pageY, 10, 0, 2 * Math.PI);
