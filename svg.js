b7961e3f02be7e606e3e9d11249b6d5a227af79a
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-12-03 12:42:48 +0200

    Update svg.js

--- svg.js
+++ svg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.12.03.2";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.12.03.4";
 window.addEventListener("deviceorientation", on_device_orientation);
 //window.addEventListener("devicemotion", on_device_motion);
 
@@ -6,6 +6,8 @@ function on_device_orientation(e){
 	var svg=document.getElementById("id_svg");
 	var circle=document.getElementById("id_circle");
 	var R=20;
+	var svg_width = svg.getAttribute("width");
+	var svg_height = svg.getAttribute("height");
 	circle.setAttribute("cx", svg.width/2+e.gamma/90*(svg.width/2-R)+"px");
 	circle.setAttribute("cy", svg.height/2+e.beta/90*(svg.height/2-R)+"px");
 }
