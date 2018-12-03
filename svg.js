8f050542ad1a0d9a6686b3d4a69d3639e46855d0
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-12-03 12:32:38 +0200

    Update svg.js

--- svg.js
+++ svg.js
@@ -7,5 +7,5 @@ function on_device_orientation(e){
 	var circle=document.getElementById("id_circle");
 	var R=20;
 	circle.setAttribute("cx", svg.width/2+e.gamma/90*(svg.width/2-R)+"px");
-	circle.setAttribute("cy", svg.height/2+e.beta/90*(svg.height/2-R)+"py");
+	circle.setAttribute("cy", svg.height/2+e.beta/90*(svg.height/2-R)+"px");
 }
