d96bc4903399c072119b130df21be1f88529dcdf
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-12-03 12:24:46 +0200

    us

--- sbg.js
+++ sbg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.12.03.0";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.12.03.1";
 window.addEventListener("deviceorientation", on_device_orientation);
 //window.addEventListener("devicemotion", on_device_motion);
 
