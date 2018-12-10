c74669c2574bc78899c5722348cfa47541b26acb
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-12-10 12:46:54 +0200

    Update gps.js

--- gps.js
+++ gps.js
@@ -1,8 +1,8 @@
-document.getElementById("id_logic_version").innerHTML="Bussiness version: 2018.12.10.0";
+document.getElementById("id_logic_version").innerHTML="Bussiness version: 2018.12.10.1";
 document.getElementById("id_start").addEventListener("click", get_position);
 
 function get_position(){
-	navigator.geolocation.getPosition(on_gps_ok, on_error);
+	navigator.geolocation.getCurrentPosition(on_gps_ok, on_error);
 }
 
 function on_gps_ok(e){
