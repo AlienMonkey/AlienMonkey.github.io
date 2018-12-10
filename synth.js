a1f2801b049b722dc15760a31dbb1dd2f9b4286d
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2018-12-10 12:31:29 +0200

    speak

---
+++ synth.js
@@ -0,0 +1,27 @@
+document.getElementById("id_logic_version").innerHTML="Bussiness version: 2018.12.10.0";
+document.getElementById("id_get_voices").addEventListener("click", get_voices);
+document.getElementById("id_start").addEventListener("click", speak);
+
+var synth = window.speechSynthesis;
+
+function get_voices(){
+	var voices = synth.getVoices();
+	var voices_list = document.getElementById("id_voices_list");
+	for(var i=0; i<synth.getVoices().length; i++){
+		voices_list.innerHTML += voices[i].lang + " " + voices[i].name + "<br>";
+	}
+}
+
+function speak(){
+	var utterance = new SpeechSynthesisUtterance();
+	utterance.lang = "en-US";
+	utterance.text = document.getElementById("id_text").value;
+	utterance.onend = on_end_speach;
+	document.getElementById("id_start").disabled = true;
+	
+	synth.speak(utterance);
+}
+
+function on_end_speach(){
+	document.getElementById("id_start").disabled = false;
+}
