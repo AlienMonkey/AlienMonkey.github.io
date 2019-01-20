5f089c1f5ba6b8b6d8d50af6ac2ee6b2d6c507c3
AlienMonkey <44159756+AlienMonkey@users.noreply.github.com>
2019-01-20 23:55:00 +0200

    Update touch.js

--- touch.js
+++ touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.7";
+document.getElementById("id_bussiness_version").innerHTML="Bussiness version: 2018.11.26.8";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
@@ -33,6 +33,6 @@ function on_touch_move(e){
 		context.arc(e.changedTouches[i].pageX - canvas_boundig_rect.left,
 					e.changedTouches[i].pageY - canvas_boundig_rect.top, 10, 0, 2 * Math.PI);
 		context.stroke();
-		
+
 	}
 }
